0be306fce5c279130151f8ed9de89f7b261f6fe6
FERGOGA <fgonzalez@fgoga.com>
2026-03-02 20:38:38 -0500

    Swagger Config

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,5 +1,6 @@
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const path = require('path');
 
 const swaggerDefinition = {
     openapi: '3.0.0',
@@ -13,6 +14,10 @@ const swaggerDefinition = {
     },
     servers: [
         {
+            url: 'https://equaly-api.vercel.app',
+            description: 'Production server',
+        },
+        {
             url: 'http://localhost:3000',
             description: 'Local development server',
         },
@@ -21,13 +26,19 @@ const swaggerDefinition = {
 
 const options = {
     swaggerDefinition,
-    apis: ['./src/routes/*.js', './src/models/*.js'], // Paths to files containing OpenAPI definitions
+    apis: [path.join(__dirname, '../routes/*.js')], // Absolute path for Vercel
 };
 
 const swaggerSpec = swaggerJSDoc(options);
 
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css";
+
 const setupSwagger = (app) => {
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+        customCss:
+            '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center !important; display: flex !important; flex-wrap: wrap !important; gap: 0 10px !important; padding: 0 10px !important; width: 100% !important; }',
+        customCssUrl: CSS_URL,
+    }));
 };
 
 module.exports = setupSwagger;
